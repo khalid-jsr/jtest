b319a587da51bb25ca6af4dccead94cfa367913e
khalid-jsr <akibuki@gmail.com>
2024-08-25 09:27:55 +0600

    GUID improvement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) {
 //        NonAsciiCharRemoval.testControlCharsRemoval();
 //        HashTest.hashPassword("campus123", "xMjnq");
-        System.out.println(Guid.getGuid());
+        //System.out.println(Guid.getGuid());
+//        System.out.println(Guid.addHyphensToUUID("c4e0b3e0-7b1b-4b1b-8b1b-9b1b1b1b1b1b"));
+        System.out.println("Original: [B0E477D57B514A5386FCDC7452946AA8], formatted: [" +Guid.formatUUID("B0E477D57B514A5386FCDC7452946AA8") + "]");
+        System.out.println("Original: [b0e477d57b514a5386fcdc7452946aa8], formatted: [" +Guid.formatUUID("b0e477d57b514a5386fcdc7452946aa8") + "]");
+        System.out.println("Original: [e91aacdb-2416-4bb6-80b5-b80c51be4a0c], formatted: [" +Guid.formatUUID("e91aacdb-2416-4bb6-80b5-b80c51be4a0c") + "]");
+        System.out.println("Original: [E91AACDB-2416-4BB6-80B5-B80C51BE4A0C], formatted: [" +Guid.formatUUID("E91AACDB-2416-4BB6-80B5-B80C51BE4A0C") + "]");
+        System.out.println("Original: [E91AACDB-2416-4BB6-80B5-B80C51B], formatted: [" + Guid.formatUUID("E91AACDB-2416-4BB6-80B5-B80C51B") + "]");
+        System.out.println("Original: [NULL], formatted: [" + Guid.formatUUID(null) + "]");
     }
 
 }
--- src/main/java/me/test/Guid.java
+++ src/main/java/me/test/Guid.java
@@ -1,11 +1,38 @@
 package me.test;
 
-import java.util.UUID;
-
 
 public class Guid {
 
     public static String getGuid() {
-        return UUID.randomUUID().toString();
+        return java.util.UUID.randomUUID().toString();
     }
+
+    /*public static String addHyphensToUUID(String uuid) {
+        if (uuid == null || uuid.isEmpty() || uuid.length() != 32) {
+            return new UUID.randomUUID().toString();
+        }
+
+        return uuid.substring(0, 8) + "-" + uuid.substring(8, 12) + "-" + uuid.substring(12, 16) + "-" + uuid.substring(16, 20) + "-" + uuid.substring(20);
+    }*/
+
+    // create a function that adds hyphens to a UUID
+    // first make the input lowercase
+    // if the input is matches regex for a UUID with hyphens, return it
+    // else if the input matches regex for a UUID without hyphens, add hyphens and return it
+    // else generate a new UUID and return it
+    public static String formatUUID(String input) {
+        if(null == input)
+            return java.util.UUID.randomUUID().toString().toLowerCase();
+
+        String uuid = input.toLowerCase();
+
+        if (uuid.matches("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")) {
+            return uuid;
+        } else if (uuid.matches("[0-9a-f]{32}")) {
+            return uuid.substring(0, 8) + "-" + uuid.substring(8, 12) + "-" + uuid.substring(12, 16) + "-" + uuid.substring(16, 20) + "-" + uuid.substring(20);
+        } else {
+            return java.util.UUID.randomUUID().toString();
+        }
+    }
+
 }
